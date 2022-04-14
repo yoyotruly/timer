cb6e60aebfe1480655f24bcf1f1d64fe4df14138
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-13 21:57:06 -0400

    Add timer implementation that takes input from the CLI and beeps with a x seconds of delay

---
+++ timer1.js
@@ -0,0 +1,18 @@
+const parseInput = function() {
+  const input = process.argv.slice(2);
+  
+  return input
+    .map(x => Number(x) * 1000)
+    .filter(x => (!isNaN(x) && x >= 0))
+};
+
+const timer = () => {
+  const seconds = parseInput();
+  seconds.map(
+    sec => setTimeout(() => process.stdout.write("\x07"), sec)
+  );
+}
+
+timer();
+
+// try run program with $ node timer1.js 1 2 -3 str
